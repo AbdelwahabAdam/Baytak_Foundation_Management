ab3b70909a189ffb0e4a28c5c44794a97f7ec82f
AbdelwahabAdam <abdelwahabosama.1@gmail.com>
2026-07-16 22:32:09 +0000

    fix aws security group

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -12,126 +12,59 @@
 ###############################################################
 
 resource "aws_security_group" "baytak_sg" {
-
-  name        = "${var.project_name}-${var.environment}-sg"
-
-  description = "Security Group for Baytak Production Server"
-
-  vpc_id = data.aws_vpc.default.id
-
-  #############################################################
-  # SSH
-  #############################################################
+  name        = "baytak-sg"
+  description = "Security group for Baytak server"
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
-
     description = "SSH"
-
-    from_port = 22
-
-    to_port = 22
-
-    protocol = "tcp"
-
-    cidr_blocks = [
-      var.ssh_cidr
-    ]
-
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
-  #############################################################
-  # HTTP
-  #############################################################
 
   ingress {
-
-    description = "HTTP"
-
-    from_port = 80
-
-    to_port = 80
-
-    protocol = "tcp"
-
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
-
+    description = "Application HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
-  #############################################################
-  # HTTPS
-  #############################################################
 
   ingress {
-
-    description = "HTTPS"
-
-    from_port = 443
-
-    to_port = 443
-
-    protocol = "tcp"
-
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
-
+    description = "Application HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-#############################################################
-# Kubernetes Ingress NodePort
-#############################################################
 
-ingress {
+  ingress {
+    description = "Ingress NGINX HTTP NodePort"
+    from_port   = 31080
+    to_port     = 31080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  description = "Kubernetes Ingress NodePort"
-
-  from_port = 31019
-
-  to_port = 31019
-
-  protocol = "tcp"
-
-  cidr_blocks = [
-    "0.0.0.0/0"
-  ]
-
-}
-
-  #############################################################
-  # Outbound
-  #############################################################
+  ingress {
+    description = "Ingress NGINX HTTPS NodePort"
+    from_port   = 31443
+    to_port     = 31443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
-
     description = "Allow all outbound traffic"
-
-    from_port = 0
-
-    to_port = 0
-
-    protocol = "-1"
-
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
-
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
-  #############################################################
-  # Tags
-  #############################################################
 
   tags = {
-
-    Name = "${var.project_name}-${var.environment}-sg"
-
-    Project = var.project_name
-
-    Environment = var.environment
-
-    ManagedBy = "Terraform"
-
+    Name = "baytak-sg"
   }
-
 }
